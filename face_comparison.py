b15077ae3eac1a4a7647fdc77423278408ff3bf1
Aaron Frederick <34990020+aaronfrederick@users.noreply.github.com>
2019-02-23 18:57:23 -0800

    intruder function

--- face_comparison.py
+++ face_comparison.py
@@ -1,39 +1,46 @@
-#final workflow cell
 from clarifai.rest import ClarifaiApp
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 import pickle
 
-#intantiate important objects
-app = ClarifaiApp(api_key='ENTER APIKEY HERE')
-is_face_model = app.public_models.face_detection_model
-model = app.public_models.face_embedding_model
-filename = "ENTER FILENAME HERE"
-reference_vectors = pickle.load(open('reference_vectors.pkl', 'rb'))
 
-#check to see if there is a face
-is_face_data = is_face_model.predict_by_filename(filename)
-is_face = is_face_data['outputs'][0]['data']
+def intruder(filename):
+    faces = []
+    #intantiate important objects
+    app = ClarifaiApp(api_key='ENTER APIKEY HERE')
+    is_face_model = app.public_models.face_detection_model
+    model = app.public_models.face_embedding_model
+    reference_vectors = pickle.load(open('reference_vectors.pkl', 'rb'))
 
-#if there is a face, see who it is
-if is_face:
-    test_vector = model.predict_by_filename(filename)
+    #check to see if there is a face
+    is_face_data = is_face_model.predict_by_filename(filename)
+    is_face = is_face_data['outputs'][0]['data']
 
-    list_of_vecs = test_vector['outputs'][0]['data']['regions']
-    vecs = [np.array(x['data']['embeddings'][0]['vector']) for x in list_of_vecs]
+    #if there is a face, see who it is
+    if is_face:
+        test_vector = model.predict_by_filename(filename)
 
-    for tv in vecs:
-        sims = cosine_similarity(reference_vectors, [tv])
-        names = ['Aaron', 'Ben', 'Danny', 'Steve']
+        list_of_vecs = test_vector['outputs'][0]['data']['regions']
+        vecs = [np.array(x['data']['embeddings'][0]['vector']) for x in list_of_vecs]
 
-        max_val = 0
-        for i, val in enumerate(sims):
-            if val > max_val:
-                max_val = val
-                name_index = i
+        for tv in vecs:
+            sims = cosine_similarity(reference_vectors, [tv])
+            names = ['Aaron', 'Ben', 'Danny', 'Steve']
 
-        if max_val > 0.5:
-            print(names[i])
-            break
-        else:
-            print("No Matches")
+            max_val = 0
+            for i, val in enumerate(sims):
+                if val > max_val:
+                    max_val = val
+                    name_index = i
+
+            if max_val > 0.5:
+                #print(names[i])
+                faces.append(names[i])
+            else:
+                #print("No Matches")
+                faces.append('Not a Match')
+        for s in list(set(faces)):
+            if s in names:
+                return len(faces), True
+        return len(faces), False
+    return 0, True
