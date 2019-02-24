b1b911cf08b34b3022ac87219f0850d434da4c61
Aaron Frederick <34990020+aaronfrederick@users.noreply.github.com>
2019-02-23 18:28:12 -0800

    Supports multiple faces

--- face_comparison.py
+++ face_comparison.py
@@ -5,7 +5,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 import pickle
 
 #intantiate important objects
-app = ClarifaiApp(api_key='replace later')
+app = ClarifaiApp(api_key='ENTER APIKEY HERE')
 is_face_model = app.public_models.face_detection_model
 model = app.public_models.face_embedding_model
 filename = "ENTER FILENAME HERE"
@@ -18,13 +18,22 @@ is_face = is_face_data['outputs'][0]['data']
 #if there is a face, see who it is
 if is_face:
     test_vector = model.predict_by_filename(filename)
-    tv = np.array(test_vector['outputs'][0]['data']['regions'][0]['data']['embeddings'][0]['vector'])
-    sims = cosine_similarity(reference_vectors, [tv])
-    names = ['Aaron', 'Ben', 'Danny', 'Steve']
 
-    for i, val in enumerate(sims):
-        if val > 0.7:
+    list_of_vecs = test_vector['outputs'][0]['data']['regions']
+    vecs = [np.array(x['data']['embeddings'][0]['vector']) for x in list_of_vecs]
+
+    for tv in vecs:
+        sims = cosine_similarity(reference_vectors, [tv])
+        names = ['Aaron', 'Ben', 'Danny', 'Steve']
+
+        max_val = 0
+        for i, val in enumerate(sims):
+            if val > max_val:
+                max_val = val
+                name_index = i
+
+        if max_val > 0.5:
             print(names[i])
             break
-        if i == 3:
+        else:
             print("No Matches")
